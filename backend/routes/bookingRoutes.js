e68fe9c1aac9243aaaf888399398f0841973ad07
Naina Mourya <nainam6025@gmail.com>
2026-02-04 19:44:28 +0530

    bookingRoutes.js solved conflict

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Booking from "../models/Booking.js";
+import Sitter from "../models/Sitter.js";
 import mongoose from "mongoose";
 
 const router = express.Router();
