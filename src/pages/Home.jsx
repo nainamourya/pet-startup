5b55e9f9a79d398d357a5e5ec91c2edd10f1a1c1
Naina Mourya <nainam6025@gmail.com>
2026-02-18 23:00:33 +0530

    footer removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,17 @@
 import AboutUs from "../components/layout/AboutUs";
 import CredibilityStats from "../components/layout/CredibilityStats";
-import Footer from "../components/layout/Footer";
+
 import Hero from "../components/layout/home/Hero";
 import StoryTimeline from "../components/layout/StoryTimeline";
 import Testimonials from "../components/layout/Testimonials";
 export default function Home() {
   return (
     <div className="min-h-screen">
-   <Hero/>
-   <AboutUs/>
-   <StoryTimeline/>
-   <CredibilityStats/>
-   <Testimonials/>
-  <Footer/>
-   
+      <Hero />
+      <AboutUs />
+      <StoryTimeline />
+      <CredibilityStats />
+      <Testimonials />
     </div>
   );
 }
