a1adc1fe1133ab6fb507800eed43be0c2f20f442
Naina Mourya <nainam6025@gmail.com>
2026-02-28 10:42:33 +0530

    Added SEO meta tags to homepage,stiemap and robots.txt file for better search engine indexing and visibility.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from "react-helmet-async";
 import './index.css'
 import App from './App.jsx'
 import "leaflet/dist/leaflet.css";
 createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
   <App />
+</HelmetProvider>
 )
