17bc0eb1b7ddf6463f4d605e13759be751f15d1d
Naina Mourya <nainam6025@gmail.com>
2026-02-28 10:54:06 +0530

    helemt remove and add in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from "react-helmet-async";
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import "leaflet/dist/leaflet.css";
-createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-  <App />
-</HelmetProvider>
-)
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,10 @@ import CredibilityStats from "../components/layout/CredibilityStats";
 import Hero from "../components/layout/home/Hero";
 import StoryTimeline from "../components/layout/StoryTimeline";
 import Testimonials from "../components/layout/Testimonials";
-import { Helmet } from "react-helmet-async";
+
 export default function Home() {
   
   return (
-  <>
-    <Helmet>
-    <title>Petroo – Book Trusted Pet Sitters & Cat Boarding in Mumbai</title>
-    <meta
-      name="description"
-      content="Looking for trusted pet sitters in Mumbai? Petroo connects pet parents with verified cat boarding, pet daycare, and home pet sitting services. Safe, secure and stress-free booking."
-    />
-  </Helmet>
     <div className="min-h-screen">
       <Hero />
       <AboutUs />
@@ -23,7 +15,5 @@ export default function Home() {
       <CredibilityStats />
       <Testimonials />
     </div>
-  
-  </>
   );
 }
